23714f01dbf1060b00ced9c3925df97ef75d3aad
Ankit Kumar <aaa2000kumar@gmail.com>
2021-07-21 18:41:12 +0530

    Updating_packages

--- dt.py
+++ dt.py
@@ -1,4 +1,20 @@
-import datetime
+from datetime import datetime    
+import pytz 
+tz_NY = pytz.timezone('Asia/Kolkata')
+
+def time ():
+	datetime_NY = datetime.now(tz_NY)  
+	x=str(datetime_NY.strftime("%H:%M"))
+	y=x.split(":")
+	y[0]=int(y[0])
+	if y[0]>12:
+		y.extend('pm')
+		y[0]=y[0]-12
+	else:
+		y.extend('am')
+	y[0]=str(y[0])
+	return(y)
+
 
 
 def day ():
